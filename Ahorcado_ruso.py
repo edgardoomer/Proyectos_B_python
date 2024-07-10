131f034649858df9db77df19f298bd34f927ae34
edgardoomer <edgfernando18@gmail.com>
2024-07-09 19:13:01 -0500

    Create Ahorcado_ruso.py
    Juego ahorcado en ruso:
    игра на русском

---
+++ Ahorcado_ruso.py
@@ -0,0 +1,24 @@
+def ahorcado_en_ruso():
+    palabra_rand=random.choice(datos_rusos_limpios)
+    letras_adivinadas=[]
+    while True:
+        letra=str(input("pon una letra: ")).lower()
+        if letra in letras_adivinadas:
+            print("Esta repetida")
+            continue
+        
+        letras_adivinadas.append(letra)
+        
+        if letra in palabra_rand:
+            print("muy bien tienes una")
+            palabra_oculta = ''.join([char if char in letras_adivinadas else '_' for char in palabra_rand])
+            print(f"Este es tu progreso: {palabra_oculta}")
+            if palabra_rand==palabra_oculta:
+                print("adivinaste la palabra")
+                break
+            
+        else:
+            print("letra incorrecta")
+
+
+ahorcado_en_ruso()
