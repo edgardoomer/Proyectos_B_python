3119682e2d9f14b79e4c1007abe7a6a3cb5856b0
edgardoomer <edgfernando18@gmail.com>
2024-07-09 19:23:38 -0500

    Update metodos_palindromos.py

--- metodos_palindromos.py
+++ metodos_palindromos.py
@@ -1,1 +1,14 @@
 
+def palindromo_metodo_1():
+    revision=list(input("ponga su palabra: "))
+    if revision[::-1]==revision:
+        print("si es")
+    else:
+        print("nada que ver")
+        
+def palindromo_metodo_2():
+    revision=list(input("ponga su palabra: "))
+    if list(reversed(revision))==revision:
+        print("si es")
+    else:
+        print("nada que ver")
